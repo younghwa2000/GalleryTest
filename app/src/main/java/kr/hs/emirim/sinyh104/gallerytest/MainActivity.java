971898583d81ab93a6b3297738d887cd1ed26901
YoungHwa Shin <sinyh104@e-mail.hs.kr>
2016-10-25 19:47:53 +0900

    #2 make gallery adapter

--- app/src/main/java/kr/hs/emirim/sinyh104/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/sinyh104/gallerytest/MainActivity.java
@@ -1,13 +1,97 @@
 package kr.hs.emirim.sinyh104.gallerytest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Gallery gallery;
+    ImageView imgMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gallery=(Gallery)findViewById(R.id.gallery_1);
+        imgMain=(ImageView)findViewById(R.id.imgv_main);
+        MyGalleryAdapter adapter=new MyGalleryAdapter(this);
+        gallery.setAdapter(adapter);
+    }
+
+
+    public class MyGalleryAdapter extends BaseAdapter{
+        Context context;
+        int[] posterIds={R.drawable.friend,R.drawable.harrypoter,R.drawable.kill,R.drawable.kingsmen,
+                R.drawable.mation,R.drawable.monster,R.drawable.paparoti,R.drawable.pieta,
+                R.drawable.thewebtoon,R.drawable.train};
+
+        public MyGalleryAdapter(Context context){
+            this.context=context;
+        }
+        /**
+         * How many items are in the data set represented by this Adapter.
+         *
+         * @return Count of items.
+         */
+        @Override
+        public int getCount() {
+            return posterIds.length;
+        }
+
+        /**
+         * Get the data item associated with the specified position in the data set.
+         *
+         * @param position Position of the item whose data we want within the adapter's
+         *                 data set.
+         * @return The data at the specified position.
+         */
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        /**
+         * Get the row id associated with the specified position in the list.
+         *
+         * @param position The position of the item within the adapter's data set whose row id we want.
+         * @return The id of the item at the specified position.
+         */
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        /**
+         * Get a View that displays the data at the specified position in the data set. You can either
+         * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+         * parent View (GridView, ListView...) will apply default layout parameters unless you use
+         * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+         * to specify a root view and to prevent attachment to the root.
+         *
+         * @param position    The position of the item within the adapter's data set of the item whose view
+         *                    we want.
+         * @param convertView The old view to reuse, if possible. Note: You should check that this view
+         *                    is non-null and of an appropriate type before using. If it is not possible to convert
+         *                    this view to display the correct data, this method can create a new view.
+         *                    Heterogeneous lists can specify their number of view types, so that this View is
+         *                    always of the right type (see {@link #getViewTypeCount()} and
+         *                    {@link #getItemViewType(int)}).
+         * @param parent      The parent that this view will eventually be attached to
+         * @return A View corresponding to the data at the specified position.
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imgv=new ImageView(context);
+            imgv.setLayoutParams(new Gallery.LayoutParams(200,300));
+            imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imgv.setPadding(5,5,5,5);
+            imgv.setImageResource(posterIds[position]);
+            return imgv;
+        }
     }
 }
